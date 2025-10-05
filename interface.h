efe926fb7905ff39c04a728183118aa93e911c58
Naman Aggarwal <namanaggarwal76@gmail.com>
2025-10-05 14:32:10 +0530

    Add network interface discovery module

---
+++ interface.h
@@ -0,0 +1,9 @@
+#ifndef INTERFACE_H
+#define INTERFACE_H
+
+#include <pcap.h>
+
+// Function to discover and list all network interfaces
+void discover_interfaces(char *selected_device, int *device_index);
+
+#endif // INTERFACE_H
--- main.c
+++ main.c
@@ -1,8 +1,12 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include "colors.h"
+#include "interface.h"
 
 int main() {
-    printf(COLOR_HEADER "[C-Shark]" COLOR_RESET " Started.\n");
+    char selected_device[256];
+    int device_index;
+    discover_interfaces(selected_device, &device_index);
+    printf("Selected: %s\n", selected_device);
     return 0;
 }
